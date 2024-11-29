adb704f6784aafd223a37615e8a80756f9e21512
Alexis-Sanchez-B <sanchezalexis1994@gmail.com>
2024-11-29 00:56:15 -0300

    Realizacion de solicitud al back end y despliegue de las tareas en el front

--- cliente/src/components/ListarTarea.jsx
+++ cliente/src/components/ListarTarea.jsx
@@ -1,20 +1,28 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {getTodasTareas} from '../api/tareas.api'
+import {TareaCard} from './TareaCard'
 
 export function ListarTarea() {
-  
+    /*[] inicia como un arreglo vacio */
+    const [tareas, setTareas] = useState([]);
+
   useEffect(() => {
     
     async function loadTareas(){
-        const res =  await getTodasTareas()
+        const res =  await getTodasTareas();
         console.log(res);
+
+        /*Guardamos la informacion del GET del back end */
+        setTareas(res.data);
     }
     loadTareas();
   }, [])
 
     return (
     <div>
-        Listado de Tareas
+        {tareas.map(tareas => (
+            <TareaCard key={tareas.id} tareas={tareas} />
+        ))}
     </div>
   )
 }
---
+++ cliente/src/components/TareaCard.jsx
@@ -0,0 +1,10 @@
+
+
+export function TareaCard({tareas}) {
+  return (
+    <div>
+    <h1>{tareas.titulo}</h1>
+    <p>{tareas.descripcion}</p>
+    </div>
+  )
+}
