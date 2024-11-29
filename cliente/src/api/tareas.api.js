cb57f556778bdea185388166adeae0b4f5824657
Alexis-Sanchez-B <sanchezalexis1994@gmail.com>
2024-11-29 10:32:05 -0300

    Se cera formulario para creacion de tareas

--- cliente/src/api/tareas.api.js
+++ cliente/src/api/tareas.api.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+const tareasApi = axios.create({
+    baseURL: 'http://localhost:8000/tareas/api/v1/tareas/'
+})
+
 export const getTodasTareas = () => {
-    return axios.get('http://localhost:8000/tareas/api/v1/tareas/');
+    return tareasApi.get('/')
 };
+
+export const creaTarea = (tareas) => {
+    return tareasApi.post('/',tareas)
+}
--- cliente/src/pages/TaskForm.jsx
+++ cliente/src/pages/TaskForm.jsx
@@ -1,7 +1,26 @@
+import {useForm} from 'react-hook-form'
+import {creaTarea} from '../api/tareas.api'
+import {useNavigate} from 'react-router-dom'
+
 export function TaskForm() {
+
+  const {register, handleSubmit, formState:{errors} } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async data => {
+    await creaTarea(data)
+    navigate('/tareas')
+  })
+
   return (
     <div>
-      Formulario Tareas
+     <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Ingrese Titulo" {...register("titulo",{required: true})} ></input>
+      {errors.titulo && <span>Ingrese un Titulo para la Tarea</span>}
+      <textarea rows="3" placeholder="Ingrese Descripcion" {...register("descripcion",{required: true})} ></textarea>
+      {errors.descripcion && <span>Ingrese una descripcion</span>}
+      <button>Crear Tarea</button>
+     </form>
     </div>
   )
 }
